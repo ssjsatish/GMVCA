e5ca080beffc0424936f74d9b5cc2aebc801ebfb
Satish Patel <SSJ.SATISH@GMAIL.COM>
2019-03-13 09:25:48 +0530

    problem detail

---
+++ dp/longest_increasing_subsequence.py
@@ -0,0 +1,7 @@
+'''
+Given a unsorted array, find the length of logest increasing subsequence 
+and print the longest increasing subsequence
+e.g. a = [5, 1, 6, 3, 9, 11, 2]
+length = 4
+sequence: [5, 6, 9, 11]
+'''
